17ed3c414a9266cceaa53f65c69ad7ac49e05d73
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2023-12-14 23:28:32 -0500

    update data

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
 
-# Declare the path excel path and sheet name
-path = "ProyeccionMunicipios2005_2020.xls"
-sheetName = "Mpios"
+# Declare the path csv path and sheet name
+path = "API_SP.POP.TOTL_DS2_en_csv_v2_6224560.csv"
 
 
 
-df = pd.read_excel(
+df = pd.read_csv(
     path,
-    sheet_name=sheetName,
-    skiprows=8
+    skiprows=4
 )
 
 st.write("""
@@ -19,5 +19,54 @@ st.write("""
 """)
 
 
-st.dataframe(df)
+st.dataframe(
+    df,
+    width=2000
+)
 
+country_available = df['Country Name']
+
+option = st.selectbox(
+    'Select a Country',
+    country_available,
+    index=248
+)
+
+# Get the specific country
+country_selected = df.iloc[248, ]
+
+# Get the country name
+country_name = country_selected.iloc[0]
+
+years_populations = country_selected \
+                    .iloc[4: -1] \
+                    # .astype(np.int128)
+
+# Create a Plotly figure
+fig = go.Figure()
+
+# Add a trace for the line graph
+fig.add_trace(
+    go.Scatter(
+        x = years_populations.index.to_list(),
+        y =  years_populations.to_list(),
+        mode='lines+markers',
+        name='Data'
+    )
+)
+
+# Update layout
+fig.update_layout(
+    title='Population of ' + country_name,
+    xaxis_title='years',
+    yaxis_title='Millions of people',
+    # bargap=0.1  # Space between bars
+)
+
+
+
+
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
